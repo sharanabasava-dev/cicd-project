e293fbe2720352c06ff215848db78d8f48318c74
sharana basava <sharanpower581@gmail.com>
2026-02-27 16:47:57 +0530

    initial: python app with dockerfile

---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.insert(0, './app')
+from app import app
+
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
+
+def test_health():
+    client = app.test_client()
+    response = client.get("/health")
+    assert response.status_code == 200
